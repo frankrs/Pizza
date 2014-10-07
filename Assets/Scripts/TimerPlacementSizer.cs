7597dfe24a3f442e805c39b7d287506e97ea47b2
frankrs <frankrs3d@gmail.com>
2014-10-07 12:29:12 -0400

    some GUI changes
    gui screen indipentent sizing

---
+++ Assets/Scripts/TimerPlacementSizer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+
+public class TimerPlacementSizer : MonoBehaviour {
+
+
+	public GUIText guiText;
+	public int divider = 10;
+	public int screenW;
+	public float offSetPercent;
+
+
+	// Update is called once per frame
+	void Update () {
+		screenW = Screen.width;
+		guiText.fontSize = screenW/divider;
+		guiText.pixelOffset = new Vector2((screenW * offSetPercent),guiText.pixelOffset.y);
+	}
+}
